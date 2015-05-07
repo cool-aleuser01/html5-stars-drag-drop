60d29ac9b58ba14bd3f8191c1086ad803dff6155
Roman Rädle <roman.raedle@outlook.com>
2015-05-07 14:03:22 +0200

    javascript module

--- js/logic.js
+++ js/logic.js
@@ -1,24 +1,61 @@
-// Everything inside of this function is called after the document is ready and
-// is short for $(document).ready(function() {});
+// more details about JavaScript module patterns are here:
+// http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
+var MyModule = (function($) {
+
+  var privateVar = "secret";
+  this.publicVar1 = "public1";
+
+  var privateFunction = function() {
+    console.log('Pssst! Don\' tell anyone! It is ' + privateVar + '!');
+  };
+
+  this.publicFunction = function() {
+    this.publicVar2 = "public2";
+    console.log('This is a JavaScript module pattern: ' + this.publicVar1 + ' and ' + this.publicVar2);
+  };
+
+  this.callPrivateFunction = function() {
+    privateFunction();
+    console.log('This is the private variable: ' + privateVar);
+  };
+
+  this.listElements = function() {
+    console.log($('blue'));
+    console.log($('class1'));
+    console.log($('div'));
+  };
+
+  this.manipulateElements = function() {
+    var $blue = $('#blue');
+    // single css value
+    $blue.css('color', 'orange');
+
+    // multple values at once
+    $blue.css({
+      'fontSize': '24px',
+      'width': '200px',
+      'height': '160px'
+    });
+
+    $('.class1').each(function(el) {
+      var $class1 = $(el);
+      $class1.css('background-color', 'orange');
+    });
+  };
+
+  return this;
+}).call({}, jQuery); //sweet! we can set this in an IIFE by passing in a blank object literal using the call method
+
+// example how to call module code
 $(function() {
+  console.log(MyModule);
+  MyModule.publicFunction();
+  MyModule.callPrivateFunction();
 
-  console.log($('blue'));
-  console.log($('class1'));
-  console.log($('div'));
+  MyModule.listElements();
+  MyModule.manipulateElements();
 
-  var $blue = $('#blue');
-  // single css value
-  $blue.css('color', 'orange');
-
-  // multple values at once
-  $blue.css({
-    'fontSize': '24px',
-    'width': '200px',
-    'height': '160px'
-  });
-
-  $('.class1').each(function(el) {
-    var $class1 = $(el);
-    $class1.css('background-color', 'orange');
-  });
+  // this function call will result in an error because privateFunction is not
+  // publicly visible.
+  MyModule.privateFunction();
 });
