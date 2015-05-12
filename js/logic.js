2958f80b209fd08061cf85094020239522ffefc1
Roman Rädle <roman.raedle@outlook.com>
2015-05-12 21:17:22 +0200

    bind and jQuery

--- js/logic.js
+++ js/logic.js
@@ -1,38 +1,15 @@
 var person = {
   firstName: "Basti",
   lastName: "Schweinsteiger",
-  showFullName: function(format) {
-    if (format && format === "last,first") {
-      console.log("2: " + this.lastName + ", " + this.firstName);
-    }
-    else {
-      console.log("1: " + this.firstName + " " + this.lastName);
-    }
+  showFullName: function(e) {
+    console.log('showFullName');
+    console.log(this.firstName + " " + this.lastName); // undefined undefined
   }
 };
 
-var anotherPerson = {
-  firstName: "Manuel",
-  lastName: "Neuer"
-};
+// jQuery on document ready
+$(function() {
+  $('#blue').click(person.showFullName);
 
-var yetAnotherPerson = {
-  firstName: "Mario",
-  lastName: "Götze"
-};
-
-// show full name of person
-person.showFullName();
-person.showFullName('last,first');
-
-// call function of an object with another context object
-person.showFullName.apply(anotherPerson);
-person.showFullName.apply(anotherPerson, ['last,first']);
-
-person.showFullName.call(anotherPerson);
-person.showFullName.call(anotherPerson, 'last,first');
-
-// use function of another object
-yetAnotherPerson.showFullName = person.showFullName;
-yetAnotherPerson.showFullName();
-yetAnotherPerson.showFullName('last,first');
+  $('#red').click(person.showFullName.bind(person));
+});
