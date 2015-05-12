5bd8ef97fe441a15811cb77914cbb50ff885aa6a
Roman Rädle <roman.raedle@outlook.com>
2015-05-12 22:15:58 +0200

    konva example

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,88 @@
-var salad = {
-  type: 'fruit',
-  made: new Date(),
-  by: 'Horst Lichter',
-  ingredients: ['apple', 'banana', 'lemon', 'grape', 'pear']
-};
+// Konva 2d HTML5 canvas framework: http://konvajs.github.io
+$(function() {
 
-var serialized = JSON.stringify(salad);
-console.log(serialized);
-console.log(JSON.parse(serialized));
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  var tween = null;
+
+  function addStar(layer, stage) {
+    var scale = Math.random();
+
+    var star = new Konva.Star({
+      x: Math.random() * stage.getWidth(),
+      y: Math.random() * stage.getHeight(),
+      numPoints: 5,
+      innerRadius: 30,
+      outerRadius: 50,
+      fill: '#89b717',
+      opacity: 0.8,
+      draggable: true,
+      scale: {
+        x : scale,
+        y : scale
+      },
+      rotationDeg: Math.random() * 180,
+      shadowColor: 'black',
+      shadowBlur: 10,
+      shadowOffset: {
+        x : 5,
+        y : 5
+      },
+      shadowOpacity: 0.6,
+      // custom attribute
+      startScale: scale
+    });
+
+    layer.add(star);
+  }
+  var stage = new Konva.Stage({
+    container: 'container',
+    width: width,
+    height: height
+  });
+
+  var layer = new Konva.Layer();
+  var dragLayer = new Konva.Layer();
+
+  for(var n = 0; n < 30; n++) {
+    addStar(layer, stage);
+  }
+
+  stage.add(layer, dragLayer);
+
+  stage.on('dragstart', function(evt) {
+    var shape = evt.target;
+    // moving to another layer will improve dragging performance
+    shape.moveTo(dragLayer);
+    stage.draw();
+
+    if (tween) {
+      tween.pause();
+    }
+    shape.setAttrs({
+      shadowOffset: {
+        x: 15,
+        y: 15
+      },
+      scale: {
+        x: shape.getAttr('startScale') * 1.2,
+        y: shape.getAttr('startScale') * 1.2
+      }
+    });
+  });
+
+  stage.on('dragend', function(evt) {
+    var shape = evt.target;
+    shape.moveTo(layer);
+    stage.draw();
+    shape.to({
+      duration: 0.5,
+      easing: Konva.Easings.ElasticEaseOut,
+      scaleX: shape.getAttr('startScale'),
+      scaleY: shape.getAttr('startScale'),
+      shadowOffsetX: 5,
+      shadowOffsetY: 5
+    });
+  });
+});
