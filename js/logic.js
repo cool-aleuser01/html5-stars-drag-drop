9cd7154c85bc3f3c6cfa20373c9241938534dd1d
Roman Rädle <roman.raedle@outlook.com>
2015-05-12 20:40:45 +0200

    Added example for JavaScript's this keyword and bind() function.

--- js/logic.js
+++ js/logic.js
@@ -3,37 +3,42 @@
 var AnimationModule = (function($) {
 
   /**
-   * Initialize module and register click events on div elements with animatable
-   * class set.
+   * Initialize module.
    */
   this.init = function() {
+    console.log('init');
+    console.log(this);
 
-    var $animatables = $('div.animatable');
+    // init name
+    this.name = 'Maria';
 
-    // need to set position to relative or absolute, otherwise animation does
-    // not work
-    $animatables.css('position', 'relative');
+    // call unbound function
+    unboundFunction();
 
-    // add event to animate elements
-    $animatables.on('click', function(e) {
-
-      console.log('clicked on div element');
-
-      // get the clicked element
-      var target = e.currentTarget;
-      $(target).animate({
-        left: '+=50'
-      }, 3000, function() {
-        console.log('animation complete');
-      });
-    });
+    // call bound function
+    boundFunction();
   };
+
+  /**
+   * In unbound functions this keyword usually refers to window object.
+   */
+  var unboundFunction = function() {
+    console.log('unboundFunction');
+    console.log(this); // window object
+  };
+
+  /**
+   * In bound functions this refers to the bound object.
+   */
+  var boundFunction = function() {
+    console.log('boundFunction');
+    console.log(this); // animation module object
+  }.bind(this);
 
   return this;
 }).call({}, jQuery); //sweet! we can set this in an IIFE by passing in a blank object literal using the call method
 
 // example how to call module code
 $(function() {
-  console.log(AnimationModule);
   var module = AnimationModule.init();
 });
